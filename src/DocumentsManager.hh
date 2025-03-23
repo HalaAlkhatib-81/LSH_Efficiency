361e49eb802de8df93d44f8f760efa50df939872
verenisse.famara.caceres <verenisse.famara.caceres@estudiantat.upc.edu>
2025-03-23 16:40:07 +0100

    docs: Añadida documentación doxygen

--- src/DocumentsManager.hh
+++ src/DocumentsManager.hh
@@ -1,7 +1,3 @@
-/** @file DocumentsManager.hh
-    @brief Definición de la clase para manejar los documentos.
-*/
-
 #ifndef DOCUMENTS_MANAGER_HH
 #define DOCUMENTS_MANAGER_HH
 
@@ -16,64 +12,141 @@
 
 using namespace std;
 
-/** @class DocumentsManager
-    @brief Clase para manejar los documentos.
-*/
+/**
+ * @class DocumentsManager
+ * @brief Clase para gestionar documentos y realizar operaciones sobre ellos.
+ */
 class DocumentsManager {
 public:
 
+    /**
+     * @brief Crea un texto base a partir de un archivo.
+     * @param archivo Nombre del archivo.
+     * @pre El archivo debe existir y ser accesible.
+     * @post Se crea un texto base a partir del archivo especificado.
+     */
     void creationTextBase(const string& archivo);
 
+    /**
+     * @brief Genera permutaciones de un archivo.
+     * @param archivo Nombre del archivo.
+     * @param size Tamaño de las permutaciones.
+     * @param num_permutations Número de permutaciones a generar.
+     * @pre El archivo debe existir y ser accesible. size y num_permutations deben ser mayores que 0.
+     * @post Se generan las permutaciones del archivo especificado.
+     */
     void permutationsGenerator(const string& archivo, const int& size, const int& num_permutations);
 
+    /**
+     * @brief Genera k-shingles de un archivo.
+     * @param archivo Nombre del archivo.
+     * @param k Tamaño de los k-shingles.
+     * @pre El archivo debe existir y ser accesible. k debe ser mayor que 0.
+     * @post Se generan los k-shingles del archivo especificado.
+     */
     void k_shinglesGenerator(const string& archivo, int k);
 
+    /**
+     * @brief Genera permutaciones de k-shingles de un archivo.
+     * @param archivo Nombre del archivo.
+     * @param size Tamaño de las permutaciones.
+     * @param num_permutations Número de permutaciones a generar.
+     * @param n Número de k-shingles a seleccionar.
+     * @pre El archivo debe existir y ser accesible. size, num_permutations y n deben ser mayores que 0.
+     * @post Se generan las permutaciones de k-shingles del archivo especificado.
+     */
     void k_shinglesPermutationsGenerator(const string& archivo, const int& size, const int& num_permutations, const int& n);
 
+    /**
+     * @brief Selecciona un documento a partir de un archivo.
+     * @param archivo Nombre del archivo.
+     * @return Un conjunto no ordenado de strings que representa el documento.
+     * @pre El archivo debe existir y ser accesible.
+     * @post Se selecciona el documento del archivo especificado.
+     */
     unordered_set<string> selectionDoc(const string& archivo);
 
+    /**
+     * @brief Muestra el tamaño de un documento.
+     * @param archivo Nombre del archivo.
+     * @pre El archivo debe existir y ser accesible.
+     * @post Se muestra el tamaño del documento en la salida estándar.
+     */
     static void docSize(const string& archivo);
 
 private:
 
-    /** @brief Limpia una palabra de signos de puntuación y la convierte en minúsculas
-        \pre recibe una palabra en formato string
-        \post el resultado es la palabra sin signos de puntuación ni tildes, en minúscula
-    */
-   static string limpiarPalabra(const string& palabra);
+    /**
+     * @brief Limpia una palabra eliminando signos de puntuación y convirtiendo a minúsculas.
+     * @param palabra La palabra a limpiar.
+     * @return La palabra limpia.
+     * @pre <em>Cierto</em>
+     * @post Se devuelve la palabra limpia.
+     */
+    static string limpiarPalabra(const string& palabra);
 
+    /**
+     * @brief Procesa un texto eliminando las stopwords.
+     * @param doc El documento a procesar.
+     * @param stopwords Las palabras a eliminar.
+     * @pre <em>Cierto</em>
+     * @post Se eliminan las stopwords del documento.
+     */
+    void proccessText(unordered_set<string>& doc, const unordered_set<string>& stopwords);
 
-   void proccessText(unordered_set<string>& doc, const unordered_set<string>& stopwords);
-
-
-    /** @brief Lee un archivo de texto línea por línea e inserta cada línea en un conjunto.
-        
-        @param filename Nombre del archivo a leer.
-        @param doc Conjunto donde se almacenarán las líneas del archivo.
-        
-        @pre El archivo debe existir y ser accesible.
-        @post El conjunto `doc` contiene todas las líneas del archivo sin duplicados.
-    */
+    /**
+     * @brief Lee un archivo y devuelve un conjunto no ordenado de strings.
+     * @param filename Nombre del archivo.
+     * @return Un conjunto no ordenado de strings que representa el contenido del archivo.
+     * @pre El archivo debe existir y ser accesible.
+     * @post Se devuelve el contenido del archivo como un conjunto no ordenado de strings.
+     */
     static unordered_set<string> readFileSet(const string &filename);
 
+    /**
+     * @brief Lee un archivo y devuelve un vector de strings.
+     * @param filename Nombre del archivo.
+     * @return Un vector de strings que representa el contenido del archivo.
+     * @pre El archivo debe existir y ser accesible.
+     * @post Se devuelve el contenido del archivo como un vector de strings.
+     */
     vector<string> readFileVector(const string &filename);
 
-    /** @brief Escribe un archivo de texto en un documento a parte.
-        
-        @param document Archivo a escribir.
-        
-        @pre El document es no vacío.
-        @post Un archivo txt con la info del document.
-    */
+    /**
+     * @brief Escribe un documento en un archivo.
+     * @param document El documento a escribir.
+     * @param path La ruta del archivo.
+     * @pre <em>Cierto</em>
+     * @post Se escribe el documento en el archivo especificado.
+     */
     void writeFile(const unordered_set<string>& document, const string& path);
 
-
+    /**
+     * @brief Lee un archivo y devuelve un conjunto no ordenado de k-shingles.
+     * @param filename Nombre del archivo.
+     * @return Un conjunto no ordenado de k-shingles.
+     * @pre El archivo debe existir y ser accesible.
+     * @post Se devuelve el contenido del archivo como un conjunto no ordenado de k-shingles.
+     */
     unordered_set<string> readKShinglesSet(const string &filename);
 
-
+    /**
+     * @brief Lee un archivo y devuelve una matriz de k-shingles.
+     * @param filename Nombre del archivo.
+     * @return Una matriz de k-shingles.
+     * @pre El archivo debe existir y ser accesible.
+     * @post Se devuelve el contenido del archivo como una matriz de k-shingles.
+     */
     vector<vector<string>> readKShinglesMatrix(const string &filename);
 
-
+    /**
+     * @brief Escribe k-shingles en un archivo.
+     * @param words Las palabras a escribir.
+     * @param path La ruta del archivo.
+     * @param k Tamaño de los k-shingles.
+     * @pre <em>Cierto</em>
+     * @post Se escriben los k-shingles en el archivo especificado.
+     */
     void writeKShingles(vector<string>& words, const string& path, const int& k);
 };
 
--- src/SimilarityAlgorithms.hh
+++ src/SimilarityAlgorithms.hh
@@ -13,80 +13,118 @@
 
 using namespace std;
 
+/**
+ * @class SimilarityAlgorithms
+ * @brief Clase que implementa varios algoritmos de similitud entre documentos.
+ */
 class SimilarityAlgorithms {
 public:
 
+    /**
+     * @brief Calcula la similitud de Jaccard entre dos archivos.
+     * @param archivo1 Ruta del primer archivo.
+     * @param archivo2 Ruta del segundo archivo.
+     * @pre Los archivos deben existir y ser accesibles
+     * @post Se imprime la similitud de Jaccard entre los dos archivos.
+     */
     void JaccardSimilarity(const string& archivo1, const string& archivo2);
 
+    /**
+     * @brief Calcula la firma MinHash de dos archivos.
+     * @param archivo1 Ruta del primer archivo.
+     * @param archivo2 Ruta del segundo archivo.
+     * @param T Número de funciones hash a utilizar.
+     * @pre Los archivos deben existir y ser accesibles
+     * @post Se imprime la similitud MinHash entre los dos archivos.
+     */
     void minHash(const string& archivo1, const string& archivo2, int& T);
 
+    /**
+     * @brief Calcula la similitud utilizando Locality Sensitive Hashing (LSH).
+     * @param archivo1 Ruta del primer archivo.
+     * @param archivo2 Ruta del segundo archivo.
+     * @param T Número de funciones hash a utilizar.
+     * @param b Número de bandas a utilizar en LSH.
+     * @pre Los archivos deben existir y ser accesibles
+     * @post Se imprime la similitud LSH entre los dos archivos.
+     */
     void LSH(const string& archivo1, const string& archivo2, int& T, int& b);
 
 private:
 
+    /**
+     * @brief Calcula la similitud entre dos conjuntos de documentos.
+     * @param doc1 Conjunto de documentos del primer archivo.
+     * @param doc2 Conjunto de documentos del segundo archivo.
+     * @return La similitud entre los dos conjuntos de documentos.
+     * @pre Los conjuntos de documentos no deben estar vacíos
+     * @post Devuelve un valor entre 0 y 1 que representa la similitud.
+     */
     double computeSimilarity(unordered_set<string>& doc1, unordered_set<string>& doc2);
 
-
-    /** @brief calcula los minHash de cada shingle existente en el documente
-     * @param shingles conjunto de shingles del documento
-     * @return vector con los minHash de los shingles
-     * @pre shingles no vacio
-     * @post se devuelven los minHash de los shingles en un vector de enteros
+    /**
+     * @brief Calcula la firma MinHash de un conjunto de shingles.
+     * @param shingles Conjunto de shingles.
+     * @param T Número de funciones hash a utilizar.
+     * @return Vector con las firmas MinHash.
+     * @pre El conjunto de shingles no debe estar vacío
+     * @post Devuelve un vector con las firmas MinHash.
      */
     static vector<uint32_t> computarMinHash(unordered_set<string> shingles, int& T);
 
-
-    /** @brief funcion general para calcular la similaridad entre dos documentos
-     * @param docA nombre del archivo del documento A
-     * @param docB nombre del archivo del documento B
-     * @param option opcion para leer el archivo
-     * @param k numero de shingles por grupo
-     * @return similaridad entre los documentos
-     * @pre docA y docB existen
-     * @post se devuelve la similaridad entre los documentos
+    /**
+     * @brief Calcula la similitud MinHash entre dos conjuntos de shingles.
+     * @param shinglesA Conjunto de shingles del primer archivo.
+     * @param shinglesB Conjunto de shingles del segundo archivo.
+     * @param T Número de funciones hash a utilizar.
+     * @return La similitud MinHash entre los dos conjuntos de shingles.
+     * @pre Los conjuntos de shingles no deben estar vacíos
+     * @post Devuelve un valor entre 0 y 1 que representa la similitud.
      */
     static double funcion_general_minHash(unordered_set<string>& shinglesA, unordered_set<string>& shinglesB, int& T);
 
-
-    /** @brief calcula las bandas de los minHash
-     * @param minhashes vector con los minHash del documento
-     * @param b numero de bandas
-     * @return vector con las bandas de los minHash
-     * @pre minhashes no vacio
-     * @post se devuelven las bandas de los minHash en un vector de vectores de enteros
+    /**
+     * @brief Divide las firmas MinHash en bandas.
+     * @param minhashes Vector con las firmas MinHash.
+     * @param T Número de funciones hash a utilizar.
+     * @param b Número de bandas a utilizar.
+     * @return Matriz con las bandas.
+     * @pre El vector de firmas MinHash no debe estar vacío
+     * @post Devuelve una matriz con las bandas.
      */
     static vector<vector<int>> bandes(vector<uint32_t> minhashes, int& T, int& b);
 
-    /** @brief calcula la similaridad de Jaccard aproximada entre dos documentos usando los minHash creados
-     * @param firmaA vector con los minHash del documento A
-     * @param firmaB vector con los minHash del documento B
-     * @return similaridad de Jaccard aproximada entre los documentos
-     * @pre firmaA y firmaB no vacios
-     * @post se devuelve la similaridad de Jaccard aproximada entre los documentos
+    /**
+     * @brief Calcula la similitud de Jaccard entre dos firmas MinHash.
+     * @param firmaA Firma MinHash del primer archivo.
+     * @param firmaB Firma MinHash del segundo archivo.
+     * @param T Número de funciones hash a utilizar.
+     * @return La similitud de Jaccard entre las dos firmas MinHash.
+     * @pre Los vectores de firmas MinHash no deben estar vacíos
+     * @post Devuelve un valor entre 0 y 1 que representa la similitud.
      */
     static double similitudJaccard(const vector<uint32_t> &firmaA, const vector<uint32_t> &firmaB, int& T);
 
-
-
-    /** @brief guarda en una tabla hash los documentos que coinciden en una banda
-     * @param bandes vector con las bandas de los minHash
-     * @param docId id del documento
-     * @param lshTable tabla hash donde se guardaran los documentos que coinciden en una banda
-     * @pre bandes no vacio
+    /**
+     * @brief Realiza el hashing sensible a la localidad (LSH) de las bandas.
+     * @param bandes Matriz con las bandas.
+     * @param docId Identificador del documento.
+     * @param lshTable Tabla hash para almacenar los resultados de LSH.
+     * @pre La matriz de bandas no debe estar vacía
+     * @post La tabla hash se actualiza con los resultados de LSH.
      */
     static void localitySensitiveHashing(const vector<vector<int>> &bandes, int docId, unordered_map<size_t, vector<int>> &lshTable);
-    
-    /** @brief funcion general para calcular la similaridad entre dos documentos
-     * @param docA nombre del archivo del documento A
-     * @param docB nombre del archivo del documento B
-     * @param option opcion para leer el archivo
-     * @param k numero de shingles por grupo
-     * @param b numero de bandas
-     * @return similaridad entre los documentos
-     * @pre docA y docB existen
-     * @post se devuelve la similaridad entre los documentos
-     */
 
+    /**
+     * @brief Calcula la similitud utilizando LSH entre dos conjuntos de shingles.
+     * @param shinglesA Conjunto de shingles del primer archivo.
+     * @param shinglesB Conjunto de shingles del segundo archivo.
+     * @param T Número de funciones hash a utilizar.
+     * @param b Número de bandas a utilizar.
+     * @return La similitud LSH entre los dos conjuntos de shingles.
+     * @pre Los conjuntos de shingles no deben estar vacíos
+     * @post Devuelve un valor entre 0 y 1 que representa la similitud.
+     */
     static double funcion_general_LSH(unordered_set<string>& shinglesA, unordered_set<string>& shinglesB, int& T, int& b);
 };
 
